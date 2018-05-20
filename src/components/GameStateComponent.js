beb7fcb59612a9e8a1432b610a947e41f1b8be54
William A. O. Brown <will.b611@gmail.com>
2018-05-20 22:35:07 +0100

    Add simple component to retrieve currently winning color once.

---
+++ src/components/GameStateComponent.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class GameStateComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {'winningColor': 'Loading'}
+    }
+    componentDidMount() {
+        this.LoadWinningColor()
+            .then(res => this.setState({'winningColor': res}))
+            .catch(err => console.log("Error in getting result: " + err));
+    }
+    LoadWinningColor = async () => {
+        const response = await fetch('http://localhost:8080/game');
+        const body = await response.text();
+
+        if (response.status !== 200) {
+            throw Error(body.message)
+        }
+        return body
+    };
+
+    render() {
+        return (
+            <p className="App-state">
+                Currently winning colour is: {this.state["winningColor"]}
+            </p>
+        )
+    }
+
+}
+
+export default GameStateComponent;
