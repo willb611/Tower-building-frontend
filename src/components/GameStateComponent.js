06ef3949f2d5d0999e05b76711195d6c7bffda80
William A. O. Brown <will.b611@gmail.com>
2018-05-29 16:40:47 +0100

    Change text from 'loading' to '???' until backend responds

--- src/components/GameStateComponent.js
+++ src/components/GameStateComponent.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class GameStateComponent extends Component {
     constructor(props) {
         super(props);
-        this.state = {'winningColor': 'Loading'}
+        this.state = {'winningColor': '???'}
     }
     componentDidMount() {
         this.LoadWinningColor()
